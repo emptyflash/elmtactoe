3f2f62119ecc705669a170d454197c0c623006cc
Cameron Alexander <emptyflash@gmail.com>
2016-03-31 11:41:53 -0500

    Update win logic to work for all rows and columns

--- src/TicTacToe.elm
+++ src/TicTacToe.elm
@@ -33,13 +33,105 @@ play player position board =
                 (p, s))
         board
 
+
+bothPlayers : List Player
+bothPlayers = [X, O]
+
+
+allRows : List Row
+allRows = [T, M, B]
+
+
+allColumns : List Column
+allColumns = [L, C, R]
+
+
+checkRow : Board -> Row -> Player -> Maybe Player
+checkRow board row player =
+    let
+        takenSpaces = List.filter
+            (\s ->
+                case s of
+                    ((r, _), Taken p) ->
+                        row == r && player == p
+                    _ ->
+                        False)
+            board
+    in
+        case takenSpaces of
+            [_, _, _] ->
+                Just player
+            _ ->
+                Nothing
+
+
+checkColumn : Board -> Column -> Player -> Maybe Player
+checkColumn board column player =
+    let
+        takenSpaces = List.filter
+            (\s ->
+                case s of
+                    ((_, c), Taken p) ->
+                        column == c && player == p
+                    _ ->
+                        False)
+            board
+    in
+        case takenSpaces of
+            [_, _, _] ->
+                Just player
+            _ ->
+                Nothing
+
+
+checkColumns : Board -> List (Maybe Player)
+checkColumns board =
+    List.concatMap
+        (\p ->
+            List.map 
+                (\r ->
+                    checkColumn board r p)
+                allColumns)
+        bothPlayers
+
+
+checkRows : Board -> List (Maybe Player)
+checkRows board =
+    List.concatMap
+        (\p ->
+            List.map 
+                (\r ->
+                    checkRow board r p)
+                allRows)
+        bothPlayers
+
+
+isSpaceOpen : Space -> Bool
+isSpaceOpen space =
+    case space of
+        (_, Open) ->
+            True
+        _ ->
+            False
+
+
+hasOpenSpaces : Board -> Bool
+hasOpenSpaces board =
+    List.any isSpaceOpen board
+
+
 checkWin : Board -> (Bool, Maybe Player)
 checkWin board =
-    case board of
-        (_, Taken X) :: (_, Taken X) :: (_, Taken X) :: rest ->
-            (True, Just X)
-        _ -> 
-            (False, Nothing)
+    let
+        checks = checkRows board ++ checkColumns board
+        maybePlayer = List.head <| List.filterMap identity checks
+    in
+        case maybePlayer of
+            Just _ ->
+                (True, maybePlayer)
+            Nothing ->
+                (not <| hasOpenSpaces board, maybePlayer)
+
 
 openSpace : Row -> Column -> Space
 openSpace row column =
@@ -52,8 +144,8 @@ openBoard =
         (\r -> 
             List.map
                 (openSpace r)
-                [L, C, R])
-        [T, M, B]
+                allColumns)
+        allRows
 
 --findBestPlayForPlayer : Player -> Board -> Position
 --findBestPlayForPlayer player board =
