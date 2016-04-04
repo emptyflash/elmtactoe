65d4b17e1d03d50cab507a9cc18085a7375eae4f
Cameron Alexander <emptyflash@gmail.com>
2016-04-04 15:31:28 -0500

    First stab at alpha beta pruning

--- src/App.elm
+++ src/App.elm
@@ -1,141 +1,179 @@
-module App where
+module App (..) where
 
 import TicTacToe exposing (..)
-
 import Signal exposing (Address)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
 
-type alias Model = 
-    { board: Board
-    , lastPlayer: Player
-    }
+type alias Model =
+  { board : Board
+  , lastPlayer : Player
+  }
 
-type Action 
-    = Play Position
-    | NoOp
+
+type Action
+  = Play Position
+  | NoOp
 
 
 init : Model
 init =
-    { board = openBoard
-    , lastPlayer = O
-    }
+  { board = openBoard
+  , lastPlayer = O
+  }
 
 
 update : Action -> Model -> Model
 update action model =
-    case action of
-        Play position ->
-            let
-                player = opponent model.lastPlayer
-                newBoard = play player model.board position 
-            in
-                { model 
-                | board = newBoard
-                , lastPlayer = player
-                }
-        NoOp ->
-            model
+  case action of
+    Play position ->
+      let
+        player =
+          opponent model.lastPlayer
+
+        newBoard =
+          play player model.board position
+      in
+        { model
+          | board = newBoard
+          , lastPlayer = player
+        }
+
+    NoOp ->
+      model
+
 
 borderColor : String
 borderColor =
-    "#646C8A"
+  "#646C8A"
 
-topBorder : (String, String)
+
+topBorder : ( String, String )
 topBorder =
-    ("border-top", "2px solid " ++ borderColor)
+  ( "border-top", "2px solid " ++ borderColor )
 
-leftBorder : (String, String)
+
+leftBorder : ( String, String )
 leftBorder =
-    ("border-left", "2px solid " ++ borderColor)
+  ( "border-left", "2px solid " ++ borderColor )
 
-rightBorder : (String, String)
+
+rightBorder : ( String, String )
 rightBorder =
-    ("border-right", "2px solid " ++ borderColor)
+  ( "border-right", "2px solid " ++ borderColor )
 
-bottomBorder : (String, String)
+
+bottomBorder : ( String, String )
 bottomBorder =
-    ("border-bottom", "2px solid " ++ borderColor)
+  ( "border-bottom", "2px solid " ++ borderColor )
 
-getBorders : Position -> List (String, String)
+
+getBorders : Position -> List ( String, String )
 getBorders position =
-    case position of
-        (T, L) -> [bottomBorder, rightBorder]
-        (M, L) -> [bottomBorder, rightBorder, topBorder]
-        (B, L) -> [topBorder, rightBorder]
-        (T, C) -> [leftBorder, rightBorder, bottomBorder]
-        (M, C) -> [leftBorder, rightBorder, topBorder, bottomBorder]
-        (B, C) -> [leftBorder, rightBorder, topBorder]
-        (T, R) -> [leftBorder, bottomBorder]
-        (M, R) -> [leftBorder, topBorder, bottomBorder]
-        (B, R) -> [topBorder, leftBorder]
+  case position of
+    ( T, L ) ->
+      [ bottomBorder, rightBorder ]
+
+    ( M, L ) ->
+      [ bottomBorder, rightBorder, topBorder ]
+
+    ( B, L ) ->
+      [ topBorder, rightBorder ]
+
+    ( T, C ) ->
+      [ leftBorder, rightBorder, bottomBorder ]
+
+    ( M, C ) ->
+      [ leftBorder, rightBorder, topBorder, bottomBorder ]
+
+    ( B, C ) ->
+      [ leftBorder, rightBorder, topBorder ]
+
+    ( T, R ) ->
+      [ leftBorder, bottomBorder ]
+
+    ( M, R ) ->
+      [ leftBorder, topBorder, bottomBorder ]
+
+    ( B, R ) ->
+      [ topBorder, leftBorder ]
 
 
 viewSpaceStyle : Position -> Attribute
-viewSpaceStyle position = 
-    style <| [ ("flex", "1") ] ++ getBorders position
+viewSpaceStyle position =
+  style <| [ ( "flex", "1" ) ] ++ getBorders position
+
 
 viewSpace : Address Action -> Space -> Html
-viewSpace address space = 
-    case space of
-        (position, Open) -> 
-            div 
-                [ viewSpaceStyle position
-                , onClick address <| Play position
-                ] 
-                [ ]
-        (position, Taken player) -> 
-            div [ viewSpaceStyle position ] [ text <| toString player ]
+viewSpace address space =
+  case space of
+    ( position, Open ) ->
+      div
+        [ viewSpaceStyle position
+        , onClick address <| Play position
+        ]
+        []
+
+    ( position, Taken player ) ->
+      div [ viewSpaceStyle position ] [ text <| toString player ]
+
 
 viewSpaceRowStyle : Attribute
-viewSpaceRowStyle = 
-    style
-        [ ("flex", "1")
-        , ("display", "flex")
-        ]
+viewSpaceRowStyle =
+  style
+    [ ( "flex", "1" )
+    , ( "display", "flex" )
+    ]
+
 
 viewSpaceRow : Address Action -> List Space -> Html
 viewSpaceRow address spaces =
-    div [ viewSpaceRowStyle ]
-        <| List.map (viewSpace address) spaces
+  div [ viewSpaceRowStyle ]
+    <| List.map (viewSpace address) spaces
 
 
 partitionGrid : Address Action -> List Space -> List Html
 partitionGrid address spaces =
-    case spaces of
-       [] -> [text ""]
-       _ ->
-           [viewSpaceRow address <| List.take 3 spaces] 
-             ++ (partitionGrid address <| List.drop 3 spaces)
+  case spaces of
+    [] ->
+      [ text "" ]
+
+    _ ->
+      [ viewSpaceRow address <| List.take 3 spaces ]
+        ++ (partitionGrid address <| List.drop 3 spaces)
+
 
 viewBoardStyle : Attribute
 viewBoardStyle =
-    style
-        [ ("display", "flex")
-        , ("flex-direction", "column")
-        , ("width", "500px")
-        , ("height", "500px")
-        ]
+  style
+    [ ( "display", "flex" )
+    , ( "flex-direction", "column" )
+    , ( "width", "500px" )
+    , ( "height", "500px" )
+    ]
+
 
 viewBoard : Address Action -> Board -> Html
 viewBoard address board =
-    div [ viewBoardStyle ]
-        <| partitionGrid address board
+  div [ viewBoardStyle ]
+    <| partitionGrid address board
+
 
 containerStyle =
-    style 
-        [ ("display", "flex")
-        , ("justify-content", "center")
-        ]
+  style
+    [ ( "display", "flex" )
+    , ( "justify-content", "center" )
+    ]
+
 
 view : Address Action -> Model -> Html
 view address model =
-    div [ containerStyle ]
-        [ viewBoard address model.board 
-        , minimax 0 (opponent model.lastPlayer) (opponent model.lastPlayer) model.board
-            |> toString
-            |> text
-        ]
+  div
+    [ containerStyle ]
+    [ viewBoard address model.board
+    , getBestMoveForPlayer model.board (opponent model.lastPlayer)
+        |> toString
+        |> text
+    ]
--- src/TicTacToe.elm
+++ src/TicTacToe.elm
@@ -1,255 +1,394 @@
-module TicTacToe where
+module TicTacToe (..) where
 
 import Maybe exposing (andThen)
 import List.Extra as ListE exposing (minimumBy, maximumBy)
 
 
-type Player = X | O
+type Player
+  = X
+  | O
 
-type Square = Open | Taken Player
 
-type Row = T | M | B
+type Square
+  = Open
+  | Taken Player
 
-type Column = L | C | R
 
-type alias Position = (Row, Column)
+type Row
+  = T
+  | M
+  | B
 
-type alias Space = (Position, Square)
 
-type alias Board = List Space
+type Column
+  = L
+  | C
+  | R
 
-type WinState = Playing | Win Player | Draw
+
+type alias Position =
+  ( Row, Column )
+
+
+type alias Space =
+  ( Position, Square )
+
+
+type alias Board =
+  List Space
+
+
+type WinState
+  = Playing
+  | Win Player
+  | Draw
 
 
 opponent : Player -> Player
 opponent player =
-    case player of
-        X -> O
-        O -> X
+  case player of
+    X ->
+      O
+
+    O ->
+      X
 
 
 play : Player -> Board -> Position -> Board
 play player board position =
-    List.map 
-        (\(p, s)->
-            if position == p then
-                (p, Taken player)
-            else
-                (p, s))
-        board
+  List.map
+    (\( p, s ) ->
+      if position == p then
+        ( p, Taken player )
+      else
+        ( p, s )
+    )
+    board
 
 
 bothPlayers : List Player
-bothPlayers = [X, O]
+bothPlayers =
+  [ X, O ]
 
 
 allRows : List Row
-allRows = [T, M, B]
+allRows =
+  [ T, M, B ]
 
 
 allColumns : List Column
-allColumns = [L, C, R]
+allColumns =
+  [ L, C, R ]
 
 
 checkRow : Board -> Row -> Player -> Maybe Player
 checkRow board row player =
-    let
-        takenSpaces = List.filter
-            (\s ->
-                case s of
-                    ((r, _), Taken p) ->
-                        row == r && player == p
-                    _ ->
-                        False)
-            board
-    in
-        case takenSpaces of
-            [_, _, _] ->
-                Just player
+  let
+    takenSpaces =
+      List.filter
+        (\s ->
+          case s of
+            ( ( r, _ ), Taken p ) ->
+              row == r && player == p
+
             _ ->
-                Nothing
+              False
+        )
+        board
+  in
+    case takenSpaces of
+      [ _, _, _ ] ->
+        Just player
+
+      _ ->
+        Nothing
 
 
 checkColumn : Board -> Column -> Player -> Maybe Player
 checkColumn board column player =
-    let
-        takenSpaces = List.filter
-            (\s ->
-                case s of
-                    ((_, c), Taken p) ->
-                        column == c && player == p
-                    _ ->
-                        False)
-            board
-    in
-        case takenSpaces of
-            [_, _, _] ->
-                Just player
+  let
+    takenSpaces =
+      List.filter
+        (\s ->
+          case s of
+            ( ( _, c ), Taken p ) ->
+              column == c && player == p
+
             _ ->
-                Nothing
+              False
+        )
+        board
+  in
+    case takenSpaces of
+      [ _, _, _ ] ->
+        Just player
+
+      _ ->
+        Nothing
 
 
 checkColumns : Board -> List (Maybe Player)
 checkColumns board =
-    List.concatMap
-        (\p ->
-            List.map 
-                (\r ->
-                    checkColumn board r p)
-                allColumns)
-        bothPlayers
+  List.concatMap
+    (\p ->
+      List.map
+        (\r ->
+          checkColumn board r p
+        )
+        allColumns
+    )
+    bothPlayers
 
 
 checkRows : Board -> List (Maybe Player)
 checkRows board =
-    List.concatMap
-        (\p ->
-            List.map 
-                (\r ->
-                    checkRow board r p)
-                allRows)
-        bothPlayers
+  List.concatMap
+    (\p ->
+      List.map
+        (\r ->
+          checkRow board r p
+        )
+        allRows
+    )
+    bothPlayers
 
 
 getPlayerFromSpace : Space -> Maybe Player
 getPlayerFromSpace space =
-    let
-        (_, square) = space
-    in
-        case square of
-            Taken player -> Just player
-            Open -> Nothing
+  let
+    ( _, square ) =
+      space
+  in
+    case square of
+      Taken player ->
+        Just player
+
+      Open ->
+        Nothing
 
 
 getNextPlayerFromBoard : Board -> Player -> Maybe Player
 getNextPlayerFromBoard board player =
-    let
-        nextSpace = List.head board
-        maybePlayer = nextSpace `andThen` getPlayerFromSpace
-    in
-        maybePlayer `andThen` (\nextPlayer ->
-            if nextPlayer == player then
-                Just player
-            else
-                Nothing)
+  let
+    nextSpace =
+      List.head board
+
+    maybePlayer =
+      nextSpace `andThen` getPlayerFromSpace
+  in
+    maybePlayer
+      `andThen` (\nextPlayer ->
+                  if nextPlayer == player then
+                    Just player
+                  else
+                    Nothing
+                )
 
 
 checkDiagonal : Board -> Maybe Player
 checkDiagonal board =
-    List.head board
-        `andThen` getPlayerFromSpace
-        `andThen` (getNextPlayerFromBoard <| List.drop 4 board)
-        `andThen` (getNextPlayerFromBoard <| List.drop 8 board)
+  List.head board
+    `andThen` getPlayerFromSpace
+    `andThen` (getNextPlayerFromBoard <| List.drop 4 board)
+    `andThen` (getNextPlayerFromBoard <| List.drop 8 board)
 
 
 checkAntiDiagonal : Board -> Maybe Player
 checkAntiDiagonal board =
-    List.head (List.drop 2 board)
-        `andThen` getPlayerFromSpace
-        `andThen` (getNextPlayerFromBoard <| List.drop 4 board)
-        `andThen` (getNextPlayerFromBoard <| List.drop 6 board)
+  List.head (List.drop 2 board)
+    `andThen` getPlayerFromSpace
+    `andThen` (getNextPlayerFromBoard <| List.drop 4 board)
+    `andThen` (getNextPlayerFromBoard <| List.drop 6 board)
 
 
 checkDiagonals : Board -> List (Maybe Player)
 checkDiagonals board =
-    [checkDiagonal board, checkAntiDiagonal board]
+  [ checkDiagonal board, checkAntiDiagonal board ]
 
 
 isSpaceOpen : Space -> Bool
 isSpaceOpen space =
-    case space of
-        (_, Open) ->
-            True
-        _ ->
-            False
+  case space of
+    ( _, Open ) ->
+      True
+
+    _ ->
+      False
 
 
 hasOpenSpaces : Board -> Bool
 hasOpenSpaces board =
-    List.any isSpaceOpen board
+  List.any isSpaceOpen board
 
 
 checkWin : Board -> WinState
 checkWin board =
-    let
-        checks = checkRows board ++ checkColumns board ++ checkDiagonals board
-        maybePlayer = List.head <| List.filterMap identity checks
-    in
-        case maybePlayer of
-            Just player ->
-                Win player
-            Nothing ->
-                if hasOpenSpaces board then
-                    Playing
-                else
-                    Draw
+  let
+    checks =
+      checkRows board ++ checkColumns board ++ checkDiagonals board
+
+    maybePlayer =
+      List.head <| List.filterMap identity checks
+  in
+    case maybePlayer of
+      Just player ->
+        Win player
+
+      Nothing ->
+        if hasOpenSpaces board then
+          Playing
+        else
+          Draw
 
 
 openSpace : Row -> Column -> Space
 openSpace row column =
-    ((row, column), Open)
+  ( ( row, column ), Open )
 
 
 openBoard : Board
 openBoard =
-    List.concatMap 
-        (\r -> 
-            List.map
-                (openSpace r)
-                allColumns)
-        allRows
+  List.concatMap
+    (\r ->
+      List.map
+        (openSpace r)
+        allColumns
+    )
+    allRows
 
 
 getScore : Int -> Player -> WinState -> Int
 getScore depth player winState =
-    case winState of
-        Win winner ->
-            if winner == player then
-                10 - depth
-            else
-                depth - 10
-        _ ->
-            0
+  case winState of
+    Win winner ->
+      if winner == player then
+        10 - depth
+      else
+        depth - 10
+
+    _ ->
+      0
 
 
 getPositionFromSpace : Space -> Position
 getPositionFromSpace space =
-    let (position, _) = space in position
+  let
+    ( position, _ ) =
+      space
+  in
+    position
 
 
-minimax : Int -> Player -> Player -> Board -> (Int, Maybe Position)
-minimax depth originalPlayer currentPlayer board =
-    let
-        availableSpaces = 
-            List.filter isSpaceOpen board
-        initialWinState = checkWin board
-    in
-        case (List.length availableSpaces, initialWinState) of
-            (9, _) -> (10, Just (B, R))
-            (_, Win _) ->
-                (getScore depth originalPlayer initialWinState, Nothing)
-            (_, Draw) ->
-                (0, Nothing)
-            _ ->
-            let
-                availablePositions = 
-                    List.map getPositionFromSpace availableSpaces
-                scoresAndMoves =
-                    List.map
-                        (\position ->
-                        let
-                            newBoard = play currentPlayer board position
-                            winState = checkWin newBoard
-                        in
-                            case winState of
-                                Playing ->
-                                    minimax (depth - 1) originalPlayer (opponent currentPlayer) newBoard
-                                _ ->
-                                    (getScore depth originalPlayer winState, Just position)
-                        )
-                        availablePositions 
-            in
-                if originalPlayer == currentPlayer then
-                    Maybe.withDefault (0, Nothing) <| minimumBy fst scoresAndMoves
-                else
-                    Maybe.withDefault (0, Nothing) <| maximumBy fst scoresAndMoves
+type FoldValue value
+  = KeepGoing value
+  | StopEarly value
+
+
+minimax : Int -> Int -> Int -> Player -> Player -> Board -> ( Int, Maybe Position )
+minimax depth min max originalPlayer currentPlayer board =
+  let
+    availableSpaces =
+      List.filter isSpaceOpen board
+
+    initialWinState =
+      checkWin board
+  in
+    case initialWinState of
+      Win _ ->
+        ( getScore depth originalPlayer initialWinState, Nothing )
+
+      Draw ->
+        ( 0, Nothing )
+
+      _ ->
+        let
+          availablePositions =
+            List.map getPositionFromSpace availableSpaces
+
+          startingValue =
+            if originalPlayer == currentPlayer then
+              ( KeepGoing min, Nothing )
+            else
+              ( KeepGoing max, Nothing )
+
+          scoreAndMove =
+            List.foldr
+              (\position foldValue ->
+                let
+                  newBoard =
+                    play currentPlayer board position
+
+                  winState =
+                    checkWin newBoard
+                in
+                  case winState of
+                    Playing ->
+                      if originalPlayer == currentPlayer then
+                        case foldValue of
+                          ( KeepGoing lastScore, lastPosition ) ->
+                            let
+                              ( score, _ ) =
+                                minimax
+                                  (depth + 1)
+                                  lastScore
+                                  max
+                                  originalPlayer
+                                  (opponent currentPlayer)
+                                  newBoard
+                            in
+                              if score > lastScore then
+                                if score > max then
+                                  ( StopEarly max, lastPosition )
+                                else
+                                  ( KeepGoing score, Just position )
+                              else
+                                ( KeepGoing lastScore, lastPosition )
+
+                          ( StopEarly lastValue, lastPosition ) ->
+                            ( StopEarly lastValue, lastPosition )
+                      else
+                        case foldValue of
+                          ( KeepGoing lastScore, lastPosition ) ->
+                            let
+                              ( score, _ ) =
+                                minimax
+                                  (depth + 1)
+                                  min
+                                  lastScore
+                                  originalPlayer
+                                  (opponent currentPlayer)
+                                  newBoard
+                            in
+                              if score < lastScore then
+                                if score < min then
+                                  ( StopEarly min, lastPosition )
+                                else
+                                  ( KeepGoing score, Just position )
+                              else
+                                ( KeepGoing lastScore, lastPosition )
+
+                          ( StopEarly lastValue, lastPosition ) ->
+                            ( StopEarly lastValue, lastPosition )
+
+                    _ ->
+                      ( KeepGoing <| getScore depth originalPlayer winState
+                      , Just position
+                      )
+              )
+              startingValue
+              availablePositions
+        in
+          case scoreAndMove of
+            ( StopEarly score, Just position ) ->
+              ( score, Just position )
+
+            ( KeepGoing score, Just position ) ->
+              ( score, Just position )
+
+            ( _, Nothing ) ->
+              ( 0, Nothing )
+
+
+getBestMoveForPlayer : Board -> Player -> Maybe Position
+getBestMoveForPlayer board player =
+  snd <| minimax 0 -10000 10000 player player board
